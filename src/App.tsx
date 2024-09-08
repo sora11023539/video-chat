53a0f4b9b03b8af4331ed2f55a1c2d0f78badc14
sora11023539 <sora11023539@gmail.com>
2024-09-08 05:55:10 -0700

    add: video calling function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  CallControls,
+  CallingState,
+  ParticipantView,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  StreamVideoParticipant,
+  useCallStateHooks,
+  User
+} from "@stream-io/video-react-sdk";
 
-function App() {
-  const [count, setCount] = useState(0)
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyLzQtTE9NIiwidXNlcl9pZCI6IjQtTE9NIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MjU3Mjk2MzgsImV4cCI6MTcyNjMzNDQzOH0.ZEHfeynJYBynwkVJ6BlBXYvS1FB9pL_r7IWTMgPLq9Y';
+const userId = '4-LOM';
+const callId = 'sBz5jeJnSBQv';
+
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+}
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+call.join({ create: true });
+
+export const MyUILayout = () => {
+  const {
+    useCallCallingState,
+  } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <StreamTheme style={{ position: 'relative' }}>
+        <SpeakerLayout participantsBarPosition={"bottom"} />
+        <CallControls />
+      </StreamTheme>
+    </div>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  )
+}
+
+export const MyParticipantList = (props: {
+  participants: StreamVideoParticipant[];
+}) => {
+  const { participants } = props;
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '8px',
+      }}
+    >
+      {participants.map((participant) => (
+        <ParticipantView
+          muteAudio
+          participant={participant}
+          key={participant.sessionId}
+        />
+      ))}
+    </div>
+  )
+}
+
+export const MyFloatingLocalParticipant = (props: {
+  participant?: StreamVideoParticipant;
+}) => {
+  const { participant } = props;
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '15px',
+        left: '15px',
+        width: '240px',
+        height: '135px',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 10px 3px',
+        borderRadius: '12px',
+      }}
+    >
+      {participant && <ParticipantView muteAudio participant={participant} />}
+    </div>
+  )
+}
